7aab333cf0e23c7dc251f34d30b5c4a61c537b6d
HernandoMV <HernandoMV@users.noreply.github.com>
2024-04-15 14:51:07 +0000

    deploy: 408dfd312d9b9547f53c7e19b94b70d919cc92c1

--- village/__init__.py
+++
@@ -1,7 +0,0 @@
-from importlib.metadata import PackageNotFoundError, version
-
-try:
-    __version__ = version("village")
-except PackageNotFoundError:
-    # package is not installed
-    pass
--- village/log.py
+++
@@ -1,5 +0,0 @@
-def log(message, exception = None):
-    if exception is None:
-        print(message)
-    else:
-        print(message, exception)
