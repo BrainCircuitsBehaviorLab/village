9de9312ad1403565a14b5dfd9d20d2c60755e6c7
marinraf <marinraf@users.noreply.github.com>
2024-04-16 18:58:38 +0000

    deploy: 8e3b807f2e3a878cfe4ad69c7c057a0629e961c5

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev1+gc275e97',
+    VERSION: '0.1.dev1+g8e3b807.d20240416',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
