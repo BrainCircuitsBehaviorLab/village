331fd45542d7dace8b87fcbc6ca7be8c7a93ac18
HernandoMV <HernandoMV@users.noreply.github.com>
2024-04-17 08:30:16 +0000

    deploy: cdb3c8c6cc0235bb590b1113440ee9b521678a02

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.1.dev1+g8e3b807.d20240416',
+    VERSION: '0.1.dev1+gcdb3c8c.d20240417',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
