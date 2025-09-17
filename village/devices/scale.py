def790f781abbb6690ed6a3ad0395616c4ade4df
Cate8 <caterinabarezzi18@gmail.com>
2025-09-17 19:56:20 +0200

    negative weights allowed

--- village/devices/scale.py
+++ village/devices/scale.py
@@ -63,7 +63,7 @@ class Scale(ScaleProtocol):
             # print(self.offset)
             # print(self.calibration)
             value = (self.get_value() - self.offset) / self.calibration
-            return value if value >= 0 else 0.0
+            return value
 
         except Exception:
             if self.error_message_timer.has_elapsed():
