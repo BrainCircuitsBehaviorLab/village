ca6df14ec6f180686930f21fa7c74b8dbd32a460
Rafael Marín <marinraf@gmail.com>
2026-04-14 20:17:02 +0200

    changes in website

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -159,67 +159,30 @@ exclude_patterns = [
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-# html_theme = "sphinxawesome_theme"
-# html_theme = "sphinx_book_theme"
-html_theme = "furo"
+html_theme = "pydata_sphinx_theme"
 
 html_title = "Training village"
-
-
-github_icon = (
-    '<svg height="26px" style="margin-top:-2px;display:inline" '
-    'viewBox="0 0 45 44" '
-    'fill="currentColor" xmlns="http://www.w3.org/2000/svg">'
-    '<path fill-rule="evenodd" clip-rule="evenodd" '
-    'd="M22.477.927C10.485.927.76 10.65.76 22.647c0 9.596 6.223 17.736 '
-    "14.853 20.608 1.087.2 1.483-.47 1.483-1.047 "
-    "0-.516-.019-1.881-.03-3.693-6.04 "
-    "1.312-7.315-2.912-7.315-2.912-.988-2.51-2.412-3.178-2.412-3.178-1.972-1.346.149-1.32.149-1.32 "  # noqa
-    "2.18.154 3.327 2.24 3.327 2.24 1.937 3.318 5.084 2.36 6.321 "
-    "1.803.197-1.403.759-2.36 "
-    "1.379-2.903-4.823-.548-9.894-2.412-9.894-10.734 "
-    "0-2.37.847-4.31 2.236-5.828-.224-.55-.969-2.759.214-5.748 0 0 "
-    "1.822-.584 5.972 2.226 "
-    "1.732-.482 3.59-.722 5.437-.732 1.845.01 3.703.25 5.437.732 "
-    "4.147-2.81 5.967-2.226 "
-    "5.967-2.226 1.185 2.99.44 5.198.217 5.748 1.392 1.517 2.232 3.457 "
-    "2.232 5.828 0 "
-    "8.344-5.078 10.18-9.916 10.717.779.67 1.474 1.996 1.474 4.021 0 "
-    "2.904-.027 5.247-.027 "
-    "5.96 0 .58.392 1.256 1.493 1.044C37.981 40.375 44.2 32.24 44.2 "
-    '22.647c0-11.996-9.726-21.72-21.722-21.72" '
-    'fill="currentColor"/></svg>'
-)
 
 html_logo = "_static/logo.png"
 
 html_theme_options = {
-    # "secondary_sidebar_items": {
-    #     # Para quitar en todas las páginas el toc: "**": [],
-    #     # Para quitarlo en alguna página en concreto: nombre_de_la_pagina: []
-    #     "**": [],
-    #     # "**": ["page-toc"],
-    #     # "index": [],
-    # },
-    # "home_page_in_toc": True,
-    # "use_download_button": False,  # True si quieres que aparezca el botón de descarga
-    # "logo_light": "_static/favicon.svg",
-    # "logo_dark": "_static/favicon-white.svg",
-    # "light_logo": "logo.png",
-    # "dark_logo": "logo.png",
-    "globaltoc_includehidden": True,
-    "body_max_width": "none",
-    # "page_width": "auto",
-    "sidebar_hide_name": True,
+    "navbar_start": ["navbar-logo"],
+    "navbar_center": ["navbar-nav"],
+    "navbar_end": ["search-button", "navbar-icon-links"],
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/BrainCircuitsBehaviorLab/village",
+            "icon": "fa-brands fa-github",
+        }
+    ],
+    "show_prev_next": True,
+    "navigation_with_keys": True,
+    "secondary_sidebar_items": ["page-toc"],
+    "show_nav_level": 2,
+    "navbar_align": "left",
 }
 html_permalinks = False
-
-# html_theme_options = {
-#     "collapse_navigation": False,
-#     "sticky_navigation": False,
-#     "navigation_depth": 4,
-#     "titles_only": False,
-# }
 
 
 # Redirect the webpage to another URL
